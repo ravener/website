8c8813ea8373cc6e4c2fbd622011bcffdcd8e81d
pollen5 <31079629+pollen5@users.noreply.github.com>
2020-05-25 13:41:22 +0430

    Build system

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Personal Website
+
+Personal website, about me and all that stuff.
+
+## Usage
+View the site at [ravener.now.sh](https://ravener.now.sh)
+
+For development: (Node.js needs to be installed)
+```sh
+$ npm run build
+```
+I use plain HTML/CSS no frameworks at all. Just a manual templating to keep pages cleaner, the base template is at [src/template.html](src/template.html) and all pages are under [src/pages](src/pages) running the above command generates the static HTML under `public/` and can be hosted anywhere easily.
+
+## License
+Released under the [MIT License](LICENSE)
---
+++ build.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+// Ensure public/ exists.
+if(!fs.existsSync("public")) fs.mkdirSync("public");
+
+// All pages.
+const files = fs.readdirSync(path.join("src", "pages"));
+// Base template.
+const template = fs.readFileSync(path.join("src", "template.html")).toString();
+
+const title = (s) => s[0].toUpperCase() + s.slice(1);
+
+for(const page of files) {
+  // Used the async version here to allow files to be quickly built in parallel
+  fs.readFile(path.join("src", "pages", page), (err, html) => {
+    if(err) throw err;
+
+    // Execute the template.
+    const output = template
+      .replace(/{{title}}/g, page === "index.html" ? "Home" : title(path.basename(page)))
+      .replace(/{{body}}/g, html.toString());
+
+    // Write the output.
+    fs.writeFile(path.join("public", page), output, (err) => {
+      if(err) throw err;
+      console.log(`Built ${path.join("public", page)}`);
+    });
+  });
+}

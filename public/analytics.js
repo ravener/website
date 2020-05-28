8553795728cd1c1cd35bd7cb232a5d8bf3f0ef98
pollen5 <31079629+pollen5@users.noreply.github.com>
2020-05-28 19:42:40 +0430

    add google analytics

--- public/analytics.js
+++ public/analytics.js
@@ -4,6 +4,6 @@ if(!(window.doNotTrack === "1" || navigator.doNotTrack === "1" || navigator.doNo
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '{{ site.google_analytics }}', 'auto');
+  ga('create', 'UA-151050317-1', 'auto');
   ga('send', 'pageview');
 }

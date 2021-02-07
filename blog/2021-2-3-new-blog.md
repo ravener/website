bb3c28a076b8bb8190864d048cbfe404628422a3
ravener <ravener.anime@gmail.com>
2021-02-07 20:25:35 +0430

    blog

---
+++ blog/2021-2-3-new-blog.md
@@ -0,0 +1,44 @@
+## Introduction
+[[toc]]
+
+I now have a new blog within my personal website.
+
+It is built using markdown (rendered by [`markdown-it`](https://npmjs.com/package/markdown-it)) and integrated to my custom build flow.
+
+I'll be posting stuff here whenever I feel like it. 
+
+There is still alot to improve and as I learn I'll be making the blog more advanced.
+
+I'll be updating this specific post whenever I add new features, this page is supposed to show all the powers of this blog.
+
+## Code Highlighting
+And we also get syntax highlighting ;)
+```js
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(5, 2));
+```
+Here's more:
+```c
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  return 0;
+}
+```
+Some Java?
+```java
+package com.ravener.stuff;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
+```
+## Things todo
+- Add comments
+- Add tags and make it easier to find articles.
--- build.js
+++ build.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
+const md = require("markdown-it")({
+  highlight: function(str, lang) {
+    return `<pre class="hljs" style="padding: 0.3em"><code class="lang-${lang}">${md.utils.escapeHtml(str)}</code></pre>`;
+  },
+  breaks: true,
+  html: true
+});
+
+md.use(require("markdown-it-anchor"), {
+  permalink: true,
+  permalinkBefore: true,
+  permalinkSymbol: "🔗" /* '§' */
+});
+
+md.use(require("markdown-it-table-of-contents"));
+
 // Determine if we are building for development.
 const dev = process.argv.includes("--dev");
 
@@ -8,6 +24,7 @@ if(dev) console.log("Building for development mode.");
 
 // Ensure public/ exists.
 if(!fs.existsSync("public")) fs.mkdirSync("public");
+if(!fs.existsSync("public/blog")) fs.mkdirSync("public/blog");
 
 // All pages.
 const files = fs.readdirSync(path.join("src", "pages"));
@@ -23,11 +40,10 @@ for(const page of files) {
 
     // Execute the template.
     const output = template
-      .replace(/{{title}}/g, page === "index.html" ? "Home" : title(path.parse(page).name))
-      // Unfortunately cannot use async api here, well not a big deal our site isn't that bloated to take ages.
       // This is a simple {{include file}} directive and also cares to respect indents so the output is clean.
       .replace(/([ \t]+)?{{include (.+)}}/g, (_, spaces, file) => spaces +
         fs.readFileSync(path.join("src", "partials", file + ".html")).toString().split("\n").join("\n" + spaces))
+      .replace(/{{title}}/g, page === "index.html" ? "Home" : title(path.parse(page).name))
       .replace(/([ \t]+)?{{body}}/g, (_, spaces) => spaces +
         html.toString().split("\n").join("\n" + spaces).trim())
       // {{prod <html>}} directives get commented out on development mode.
@@ -37,6 +53,44 @@ for(const page of files) {
     fs.writeFile(path.join("public", page), output, (err) => {
       if(err) throw err;
       console.log(`Built ${path.join("public", page)}`);
+    });
+  });
+}
+
+const blogTemplate = fs.readFileSync(path.join("src", "template-blog.html")).toString();
+
+// Build the blog
+const posts = fs.readdirSync("blog").map(post => ({
+  file: post,
+  title: post.slice(0, -3).split("-").slice(3).map(title).join(" "),
+  date: {
+    year: post.split("-")[0],
+    month: post.split("-")[1],
+    day: post.split("-")[2]
+  }
+}));
+
+for(const post of posts) {
+  fs.readFile(path.join("blog", post.file), (err, page) => {
+    if(err) throw err;
+
+    const html = md.render(page.toString());
+
+    // You know the drill. Maybe I should put this in a function but it's not like I'm gonna write this everyday
+    const output = blogTemplate
+      .replace(/{{date}}/g, `${post.date.year}/${post.date.month}/${post.date.day}`)
+      .replace(/([ \t]+)?{{include (.+)}}/g, (_, spaces, file) => spaces +
+        fs.readFileSync(path.join("src", "partials", file + ".html")).toString().split("\n").join("\n" + spaces))
+      .replace(/{{title}}/g, post.title)
+    //.replace(/([ \t]+)?{{body}}/g, (_, spaces) => spaces +
+    //   md.split("\n").join("\n" + spaces).trim())
+      .replace(/{{body}}/g, html)
+      .replace(/{{prod (.+)}}/g, (_, body) => dev ? `<!-- ${body} -->` : body);
+
+    const filePath = path.join("public", "blog", post.file.replace(".md", ".html"));
+    fs.writeFile(filePath, output, (err) => {
+      if(err) throw err;
+      console.log(`Built blog post ${filePath}`);
     });
   });
 }
